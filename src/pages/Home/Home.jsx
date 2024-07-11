0c459b8fdd26790c48aaf709320f13a2e656c741
MykytaKotovSS <m.kotov@sloboda-studio.com>
2024-07-11 12:57:05 +0300

    add button and finalize home main

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Header from "components/Header";
+import Button from "components/Button";
+import { ReactComponent as Logo } from "assets/icons/logo-blue.svg";
 import "./Home.scss";
 
 const Home = () => {
@@ -7,6 +9,16 @@ const Home = () => {
     <main className="home">
       <article className="main">
         <Header theme="white" />
+        <section className="top container">
+          <span>
+            <Logo />
+            PowrAI Token
+          </span>
+          <h1>
+            Empowering Crypto Investors through Sustainable Bitcoin Mining
+          </h1>
+          <Button>Learn More</Button>
+        </section>
       </article>
     </main>
   );
